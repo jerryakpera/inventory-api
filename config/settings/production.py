8b549a3efca40b7a891b81502cee6680764c3ae9
Jeremiah Akpera <jerryakpera@gmail.com>
2025-03-02 07:47:39 +0100

    Added production database url

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,4 +2,17 @@
 Settings to be used in production environment.
 """
 
+import dj_database_url
+from decouple import config
+
 from .base import *
+from .base import BASE_DIR
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
